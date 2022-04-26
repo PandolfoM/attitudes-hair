cbfb3ed9f95aefb4e8ac78597bc8797570e474de
Matthew Pandolfo <matt@pandolfo.com>
2022-04-26 19:32:17 -0400

    login to account

--- client/src/components/LoginModal/index.js
+++ client/src/components/LoginModal/index.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { LOGIN } from "../../utils/mutations";
+import Auth from "../../utils/auth";
 import {
+  Button,
   Dialog,
+  DialogActions,
   DialogContent,
+  DialogContentText,
   DialogTitle,
   IconButton,
   TextField,
@@ -10,9 +16,33 @@ import CloseIcon from "@mui/icons-material/Close";
 
 function LoginModal(props) {
   const { openLogin, setOpenLogin } = props;
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error }] = useMutation(LOGIN);
 
   const handleClose = () => {
     setOpenLogin(false);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const mutationResponse = await login({
+        variables: { email: formState.email, password: formState.password },
+      });
+      const token = mutationResponse.data.login.token;
+      Auth.login(token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+    console.log(formState);
   };
 
   return (
@@ -39,6 +69,7 @@ function LoginModal(props) {
           type="email"
           variant="standard"
           fullWidth
+          onChange={handleChange}
         />
         <br />
         <TextField
@@ -48,8 +79,17 @@ function LoginModal(props) {
           type="password"
           variant="standard"
           fullWidth
+          onChange={handleChange}
         />
+        {error ? (
+          <DialogContentText>
+            <p>The provided credentials are incorrect</p>
+          </DialogContentText>
+        ) : null}
       </DialogContent>
+      <DialogActions>
+        <Button type="submit" onClick={handleFormSubmit}>Submit</Button>
+      </DialogActions>
     </Dialog>
   );
 }
