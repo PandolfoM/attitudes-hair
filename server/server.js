c44316094d39ff45ba706dff27a946fcd3e2502b
Matthew Pandolfo <matt@pandolfo.com>
2022-04-26 14:16:40 -0400

    navbar

--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const http = require("http");
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
-const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 
