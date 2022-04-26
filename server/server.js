1b1d028bca82f8572a86b5254e68692bbed5b513
Matthew Pandolfo <matt@pandolfo.com>
2022-04-26 19:36:13 -0400

    fix server not connecting

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const path = require("path");
 const http = require("http");
+const db = require('./config/connection')
 
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
